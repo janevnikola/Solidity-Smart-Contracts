deefe016d0d37d5e93b285d0b72d4e31654074e3
janevnikola <72938546+janevnikola@users.noreply.github.com>
2022-02-23 16:29:33 +0100

    Update HelloWorld192063.sol

--- HelloWorld192063.sol
+++ HelloWorld192063.sol
@@ -5,15 +5,21 @@ pragma solidity ^0.7.0;
 contract HelloWorld192063{
 
  string public poraka;
-
+ int16 public broj1;
+int16 public broj2; //16 byten integer?
 
 constructor (string memory newPoraka)public {
     poraka=newPoraka;
+    broj1=10;
+    broj2=12;
 }
 
  function getResult() public view returns(string memory) {
     return poraka;
  }
-
+ 
+function sobiranje() public view returns(int16){
+    return broj1+broj2;
+}
 
 }
